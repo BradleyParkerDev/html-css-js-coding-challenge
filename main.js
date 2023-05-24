9178789d6a43574faf62e21d9d8696e91f43262c
Bradley Parker <Bradleyscottparker@outlook.com>
2023-05-24 16:55:08 -0500

    Count prints to the dom, need to append stars to dom as count increases

--- main.js
+++ main.js
@@ -12,12 +12,23 @@ outerContainer.appendChild(innerContainer)
 innerContainer.id ='inner-container'
 
 
-
-
 //inner-divs
 const appHeader = document.createElement('div')
 appHeader.className = "inner-divs"
 appHeader.style.backgroundColor = "red"
+const logo = document.createElement('p')
+logo.innerText = 'LOGO';
+logo.style.fontWeight =  "bold"
+const menu = document.createElement('p')
+menu.innerText = "menu";
+appHeader.appendChild(logo)
+appHeader.appendChild(menu)
+appHeader.style.justifyContent = "space-evenly"
+logo.style.marginRight = "60%"
+logo.style.marginTop = "10%"
+menu.style.marginTop = "10%"
+
+
 
 const counterDiv = document.createElement('div')
 counterDiv.className = "inner-divs"
@@ -30,6 +41,7 @@ innerContainer.appendChild(appHeader);
 innerContainer.appendChild(counterDiv);
 innerContainer.appendChild(iconDiv);
 
+
 //Star
 const star = document.createElement('div')
 star.className = "stars"
@@ -37,49 +49,59 @@ const starImage = document.createElement("IMG");
 starImage.setAttribute('src', 'star.png')
 starImage.id = "star-image";
 star.appendChild(starImage)
-iconDiv.appendChild(star)
+
 
 //Buttons
 const removeBtn = document.createElement('button')
 removeBtn.className = 'buttons'
-removeBtn.innerHTML = ('remove 1')
+removeBtn.innerHTML = ('Remove 1')
 counterDiv.appendChild(removeBtn)
 const countText = document.createElement('h3')
+countText.innerHTML = `${count}`;
+
 counterDiv.appendChild(countText);
 const addBtn = document.createElement('button')
 addBtn.className = 'buttons'
-addBtn.innerHTML = ('add 1')
+addBtn.innerHTML = ('Add 1')
 counterDiv.appendChild(addBtn)
 
+//Button Event Listeners
 removeBtn.addEventListener('click', ()=>{
-    console.log(count)
     return remove()
-
 })
 
 addBtn.addEventListener('click', ()=>{
-    console.log(count)
     return add()
-
 })
-//Counter
 
+
+
+//Counters
 const add = () => {
     
     if(count < 5){
-        return count++;
+        count++;
+        countText.innerHTML = `${count}`;
+        iconDiv.appendChild(star)
+
+        console.log(count)
+
     }
 }
 
 const remove = () => {
     
     if(count <= 5 && count > 0){
-        return count--;
+        count--;
+        countText.innerHTML = `${count}`;
+        iconDiv.removeChild(star)
+
+        console.log(count)
+
     }
 }
 
 
 
-countText.innerHTML = count;
 
 // body.style.backgroundColor = "red";
