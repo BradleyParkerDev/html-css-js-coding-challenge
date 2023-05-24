d307dbd19bec4a3f6d951796b1a0fd05087369c4
Bradley Parker <Bradleyscottparker@outlook.com>
2023-05-24 16:27:57 -0500

    added most elements and styling, counter prints to console

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let count = 0
 //Body
 const body = document.querySelector('body');
 
@@ -16,9 +17,11 @@ innerContainer.id ='inner-container'
 //inner-divs
 const appHeader = document.createElement('div')
 appHeader.className = "inner-divs"
+appHeader.style.backgroundColor = "red"
 
 const counterDiv = document.createElement('div')
 counterDiv.className = "inner-divs"
+counterDiv.style.justifyContent = "space-evenly"
 
 const iconDiv = document.createElement('div')
 iconDiv.className = "inner-divs"
@@ -27,30 +30,56 @@ innerContainer.appendChild(appHeader);
 innerContainer.appendChild(counterDiv);
 innerContainer.appendChild(iconDiv);
 
-
+//Star
+const star = document.createElement('div')
+star.className = "stars"
+const starImage = document.createElement("IMG");
+starImage.setAttribute('src', 'star.png')
+starImage.id = "star-image";
+star.appendChild(starImage)
+iconDiv.appendChild(star)
 
 //Buttons
-const decreaseBtn = document.createElement('button')
-decreaseBtn.innerHTML = ('remove 1')
-const increaseBtn = document.createElement('button')
-increaseBtn.innerHTML = ('add 1')
+const removeBtn = document.createElement('button')
+removeBtn.className = 'buttons'
+removeBtn.innerHTML = ('remove 1')
+counterDiv.appendChild(removeBtn)
+const countText = document.createElement('h3')
+counterDiv.appendChild(countText);
+const addBtn = document.createElement('button')
+addBtn.className = 'buttons'
+addBtn.innerHTML = ('add 1')
+counterDiv.appendChild(addBtn)
 
+removeBtn.addEventListener('click', ()=>{
+    console.log(count)
+    return remove()
+
+})
+
+addBtn.addEventListener('click', ()=>{
+    console.log(count)
+    return add()
+
+})
 //Counter
-let count = 0
-const counter = (param) => {
+
+const add = () => {
     
-    if(param === 'increase'){
+    if(count < 5){
+        return count++;
+    }
+}
 
-        return count ++;
-    }else if( param === 'decrease'){
-        return count --;
-
+const remove = () => {
+    
+    if(count <= 5 && count > 0){
+        return count--;
     }
 }
 
 
 
+countText.innerHTML = count;
 
-
-body.appendChild(outerDiv);
 // body.style.backgroundColor = "red";
